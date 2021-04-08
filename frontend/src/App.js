5ee8e094a82a2877437c55fd58202b744c7ab2ee
Felix <Felix@hh.de>
2021-04-08 14:11:45 +0200

    Add dummy process into frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,92 @@
+import './style.css'
+
 function App() {
   return (
     <div className="App">
-      npro
+        <div>npro</div>
+        <div className="parent">
+
+            <button className="button">Order Manager</button>
+            <button className="button">Take Orders</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+
+            <button className="button">Pizza Master</button>
+            <button className="button"/>
+            <button className="button">Form the dough</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+
+            <button className="button">Sauce Spreader</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button">Spread the sauce</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+
+            <button className="button">Pizza Assembler</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button">Assemble the pizza</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+
+            <button className="button">Topping Preparer</button>
+            <button className="button"/>
+            <button className="button">Prepare and portion ingredients</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+
+            <button className="button">Pizza Baker</button>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button"/>
+            <button className="button">Pizza ok?</button>
+            <button className="button">Correct pizza if possible</button>
+            <button className="button">Bake Pizza</button>
+            <button className="button">Pizza ok?</button>
+            <button className="button">Throw pizza away</button>
+            <button className="button">Deliver pizza</button>
+
+
+
+
+
+
+        </div>
+
+
+
 
     </div>
   );
